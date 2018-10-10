0d5fab22a8d38bef290b28e86e5e9f81a78dcc34
LLran <katsudonn@163.com>
2018-10-10 20:37:53 +0800

    fibonacci can print result in decimal format

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,9 +1,7 @@
 DATA    SEGMENT
-FL      DW  30  DUP(0)  ; lower part of result
-FH      DW  30  DUP(0)  ; high part of result
-TMP     DW  ?
-TMP1    DW  ?
-TMP2    DW  ?
+FL      DW      30  DUP(0)              ; lower part of result
+FH      DW      30  DUP(0)              ; high part of result
+TMP     DW      ?               
 DATA    ENDS
 
 CODE	SEGMENT
@@ -13,25 +11,25 @@ START:	PUSH	DS
 	SUB 	AX,AX
 	PUSH	AX
 
-        MOV     AX,DATA             ; set up for data segment
+        MOV     AX,DATA                 ; set up for data segment
         MOV     DS,AX
         MOV     SI,OFFSET FL
         MOV     DI,OFFSET FH
 
-        MOV     WORD PTR [SI],1     ; the first number of fibonacci sequence
-        MOV     WORD PTR [SI+2],1   ; the second number of fibonacci sequence
+        MOV     WORD PTR [SI],1         ; the first number of fibonacci sequence
+        MOV     WORD PTR [SI+2],1       ; the second number of fibonacci sequence
         ADD     SI,4
         ADD     DI,4
 
-        MOV     CX,28               ; loop 28 times
+        MOV     CX,28                   ; loop 28 times
 AGAIN:  MOV     AX,[SI-2]
         MOV     BX,[SI-4]
-        ADD     AX,BX               ; add lower part
+        ADD     AX,BX                   ; add lower part
         MOV     [SI],AX
 
         MOV     AX,[DI-2]
         MOV     BX,[DI-4]
-        ADC     AX,BX               ; add higher part
+        ADC     AX,BX                   ; add higher part
         MOV     [DI],AX
         
         ADD     SI,2
@@ -41,11 +39,11 @@ AGAIN:  MOV     AX,[SI-2]
         MOV     CX,30
         MOV     SI,OFFSET FL
         MOV     DI,OFFSET FH
-PRINT:  MOV     AX,[DI]
-        MOV     BX,[SI]
-        MOV     TMP1,AX
-        MOV     TMP2,BX
+PRINT:  MOV     DX,[DI]
+        MOV     AX,[SI]
+        PUSH    CX                      ; protect CX
         CALL    PRINTER
+        POP     CX                      ; recover CX
         ADD     SI,2
         ADD     DI,2
         LOOP    PRINT 
@@ -54,43 +52,42 @@ PRINT:  MOV     AX,[DI]
 MAIN	ENDP
 
 PRINTER PROC    NEAR
-        MOV     TMP,CX              ; protect original CX
+        MOV     CX,10000
+        DIV     CX
+        MOV     TMP,AX                  ; protect higher part of number
 
-        MOV     CX,16               ; loop 16 times
-        MOV     BX,TMP1
-LOOP1:  SHL     BX,1
-        JC      ONE1
-        MOV     DL,30H
-        JMP     PT1  
-ONE1:   MOV     DL,31H  
-PT1:    MOV     AH,2
-        INT     21H                 ; print binary number
-        LOOP    LOOP1
+        MOV     CX,4                    ; loop 4 times
+        MOV     AX,DX
+LOOP1:  MOV     DX,0
+        MOV     BX,10
+        DIV     BX                      ; AX = AX / 10, DX = AX % 10
+        PUSH    DX                      ; store remainder
+        LOOP    LOOP1        
 
-        MOV     DL,20H              ; print space
+        MOV     CX,2                    ; loop 2 times
+        MOV     AX,TMP                  ; recover higher part of number
+LOOP2:  MOV     DX,0
+        MOV     BX,10
+        DIV     BX                      ; AX = AX / 10, DX = AX % 10
+        PUSH    DX                      ; store remainder
+        LOOP    LOOP2   
+
+
+
+        MOV     CX,6                    ; loop 6 times to print 6 dights
+LOOP3:  POP     DX  
+        OR      DL,30H                  ; transform to ASCII
+        MOV     AH,2
+        INT     21H
+        LOOP    LOOP3
+
+        MOV     DL,0DH                  ; print \r
         MOV     AH,2
         INT     21H
 
-        MOV     CX,16               ; loop 16 times
-        MOV     BX,TMP2
-LOOP2:  SHL     BX,1
-        JC      ONE2
-        MOV     DL,30H 
-        JMP     PT2 
-ONE2:   MOV     DL,31H  
-PT2:    MOV     AH,2
-        INT     21H                 ; print binary number
-        LOOP    LOOP2
-
-        MOV     DL,0DH              ; print \r
+        MOV     DL,0AH                  ; print \n
         MOV     AH,2
         INT     21H
-
-        MOV     DL,0AH              ; print \n
-        MOV     AH,2
-        INT     21H
-
-        MOV     CX,TMP              ; recover CX
 
         RET
 PRINTER ENDP
