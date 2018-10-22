bdb413e24caeb7af795fa65eeda554a6c0a55f2b
LLipter <katsudonn@163.com>
2018-10-22 20:01:51 +0800

    first try successfully

--- AT&T-64/doNothing/doNothing.s
+++ AT&T-64/doNothing/doNothing.s
@@ -1,9 +1,9 @@
-        .text 
-        .globl  main 
-        .type   main, @function 
-        pushq   %rbp                # save caller’s frame pointer 
-        movq    %rsp, %rbp          # establish our frame pointer 
-        movl    $0, %eax            # return 0 to caller 
-        movq    %rbp, %rsp          # restore stack pointer 
-        popq     %rbp                # restore caller’s frame pointer 
-        ret 
+		.text
+		.globl	main
+		.type	main, @function
+main:	pushq	%rbp				# save caller’s frame pointer 
+		movq	%rsp, %rbp			# establish our frame pointer
+		movl	$0, %eax			# return 0 to caller
+		movq	%rbp, %rsp			# restore stack pointer
+		popq	%rbp				# restore caller’s frame pointer
+		ret
